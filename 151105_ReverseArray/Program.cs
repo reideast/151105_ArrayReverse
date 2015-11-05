20dcf1db4329e06480fd6436304945cdcbd9651a
Andrew Reid East <andrew@andreweast.net>
2015-11-05 22:33:10 +0000

    Added comments, tested for even/odd array sizes

--- 151105_ReverseArray/Program.cs
+++ 151105_ReverseArray/Program.cs
@@ -13,19 +13,20 @@ namespace _151105_ReverseArray
             //    Console.WriteLine("Enter the array elements[" + i + "]:");
             //    arr[i] = int.Parse(Console.ReadLine());
             //}
-        
-            int[] arr = new int[10]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            const int SIZE = 10;
+            int[] arr = new int[SIZE]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             //write your logic here
-            for (int i = 0, j = 9; i < 5; i++, j--) //loop through half the array
+            for (int i = 0, j = SIZE - 1; i < SIZE / 2; i++, j--) //loop through half the array
             {
-                arr[i] ^= arr[j];
+                arr[i] ^= arr[j]; //XOR swap (note: don't do this in an actual project. see: https://en.wikipedia.org/wiki/XOR_swap_algorithm#Reasons_for_avoidance_in_practice )
                 arr[j] ^= arr[i];
                 arr[i] ^= arr[j];
             }
             //end 
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < SIZE; i++)
             {
                 Console.Write(arr[i] + " ");
             }
